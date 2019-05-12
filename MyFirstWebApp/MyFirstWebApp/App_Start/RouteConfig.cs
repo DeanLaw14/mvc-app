8a3b7365011baf301e8861d7a56fdfeaa47f3e5e
Dean Akinwale <dean.akinwale@hotmail.com>
2019-05-12 13:55:15 +0100

    some routing comments added

--- MyFirstWebApp/MyFirstWebApp/App_Start/RouteConfig.cs
+++ MyFirstWebApp/MyFirstWebApp/App_Start/RouteConfig.cs
@@ -9,15 +9,25 @@ namespace MyFirstWebApp
 {
     public class RouteConfig
     {
+        // Routes define how requests are handled within the app
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // Custom route example
+            routes.MapRoute(
+                name: "testRoute",
+                url: "Home/Contact-us", 
+                defaults: new { controller = "Home", action = "Contact" }
+            );
+
+            // Determines what method - in this case the index action result from the home controller
+            // (which returns the index view) is invoked when the defined url is hit
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                url: "{controller}/{action}/{id}", // Pattern
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Handler
+            );                    
         }
     }
 }
