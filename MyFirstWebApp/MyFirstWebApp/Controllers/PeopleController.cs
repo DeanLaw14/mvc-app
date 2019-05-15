ee6c5db4124187742e8b6563d618811ed074320b
Dean Akinwale <dean.akinwale@hotmail.com>
2019-05-15 13:21:55 +0100

    added some error comments in the web.config

--- MyFirstWebApp/MyFirstWebApp/Controllers/PeopleController.cs
+++ MyFirstWebApp/MyFirstWebApp/Controllers/PeopleController.cs
@@ -17,11 +17,12 @@ namespace MyFirstWebApp.Controllers
 
         public ActionResult ListPeople()
         {
-            var people = new List<PersonModel>();
-
-            people.Add(new PersonModel { FirstName = "Dean", LastName= "Akinwale", Age = 27, IsActive = false });
-            people.Add(new PersonModel { FirstName = "Sis", LastName= "Akinwale", Age = 29, IsActive = true });
-            people.Add(new PersonModel { FirstName = "Bro", LastName= "Akinwale", Age = 25, IsActive = true });
+            var people = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "Dean", LastName = "Akinwale", Age = 27, IsActive = false },
+                new PersonModel { FirstName = "Sis", LastName = "Akinwale", Age = 29, IsActive = true },
+                new PersonModel { FirstName = "Bro", LastName = "Akinwale", Age = 25, IsActive = true }
+            };
 
             return View(people);
         }
